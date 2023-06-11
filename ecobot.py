928f8e56530e360b30a15797e316e8e57524cca7
yernur <123850340+yernur0@users.noreply.github.com>
2023-06-11 10:58:04 +0500

    Add files via upload

--- ecobot.py
+++ ecobot.py
@@ -49,5 +49,37 @@ async def helpp(ctx):
     ecolink - Полезные сайты
     animal - То что мы стараемся защитить''')
 
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+def get_fox_image_url():    
+    url = 'https://randomfox.ca/floof/'
+    res = requests.get(url)
+    data = res.json()
+    return data['image']
+
+#block of random animals
+@bot.command()
+async def animal(ctx):
+    animals = ['duck', 'dog', 'fox']
+    random_animal = random.choice(animals)
+
+    if random_animal == 'duck':
+        image_url = get_duck_image_url()
+    elif random_animal == 'dog':
+        image_url = get_dog_image_url()
+    elif random_animal == 'fox':
+        image_url = get_fox_image_url()
+
+    await ctx.send(image_url)
 
 bot.run("token")
